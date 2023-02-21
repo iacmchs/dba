b089fef8c153c95c9aa044af076921e10e579a2f
u.chebotok <chebotokyd@mchs.ru>
2023-02-21 14:32:11 +0300

    DBA-003: Added example for pdo_* connection

--- src/Infrastructure/DBConnector.php
+++ src/Infrastructure/DBConnector.php
@@ -16,8 +16,8 @@ class DBConnector
 
     /**
      * Create connection to DB
-     * Example dsn: pgsql://user:password@127.0.0.1:5432/database
-     *
+     * Example dsn : pgsql://user:password@127.0.0.1:5432/database
+     *               pdo-pgsql://user:password@127.0.0.1:5432/database
      * @param string $dsn
      *
      * @return Connection
