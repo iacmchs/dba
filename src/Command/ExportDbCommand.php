3a4b316a49d3b3c2b9cc036d48601810c0e81a41
u.chebotok <chebotokyd@mchs.ru>
2023-03-28 19:11:03 +0300

    DBA-2: Edit app:db-export description and dsn argument

--- src/Command/ExportDbCommand.php
+++ src/Command/ExportDbCommand.php
@@ -24,7 +24,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:db-export',
-    description: 'Dump DB structure to folder <DATABASE_DUMP_FOLDER>/<database-name_Ymd_His>/00_<database-name>_structure.sql SQL file. DATABASE_DUMP_FOLDER is exposed in ENV file.'
+    description: 'Create DB dump to folder <DATABASE_DUMP_FOLDER>/<database-name_Ymd_His>. See DATABASE_DUMP_FOLDER in ENV file.'
 )]
 class ExportDbCommand extends Command
 {
@@ -44,7 +44,7 @@ class ExportDbCommand extends Command
             ->addArgument(
                 'dsn',
                 InputArgument::REQUIRED,
-                'dsn should match the pattern: "driver://user:password@host:port/database"'
+                'Should match the pattern: "driver://user:password@host:port/database"'
             );
     }
 
