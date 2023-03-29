4ef8d965e73084758e5daeff95345df1b6663ffd
u.chebotok <chebotokyd@mchs.ru>
2023-03-29 13:58:43 +0300

    DBA-3: Remove unused .gitlab scripts and hooks

--- .gitlab/_scripts/init-hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [ ! -f .git/hooks/commit-msg ]; then
-    ln -s ../../.gitlab/_hooks/commit-msg .git/hooks
-fi
-if [ ! -f .git/hooks/pre-push ]; then
-    ln -s ../../.gitlab/_hooks/pre-push .git/hooks
-fi
