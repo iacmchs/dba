ee75d2e6310c68df537c5459eafc77734c7ded9c
u.chebotok <chebotokyd@mchs.ru>
2023-03-29 16:32:44 +0300

    DBA-3: Remove spaces between concatenation operators in PostgresDbStructureExtractor:getNewStructureFileName

--- src/Service/DDL/Extractor/PostgresDbStructureExtractor.php
+++ src/Service/DDL/Extractor/PostgresDbStructureExtractor.php
@@ -221,7 +221,7 @@ class PostgresDbStructureExtractor implements
      */
     private function getNewStructureFileName(string $name): string
     {
-        return '00_' . $name . '_structure.sql';
+        return '00_'.$name.'_structure.sql';
     }
 
     /**
