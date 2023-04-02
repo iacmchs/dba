0f6f8c2a3c06e20d2a4bb7b40b988c39e8c3d162
u.chebotok <chebotokyd@mchs.ru>
2023-04-02 23:46:43 +0300

    DBA-4: Move DbConnectionSetterInterface file info to interface comment zone

--- src/Service/DbConnectionSetterInterface.php
+++ src/Service/DbConnectionSetterInterface.php
@@ -1,18 +1,15 @@
 <?php
 
-/**
- * @file
- * DB connection setter.
- *
- * The interface should be implemented only for classes in which db connection cannot be injected through constructor.
- */
-
 declare(strict_types=1);
 
 namespace App\Service;
 
 use Doctrine\DBAL\Connection;
 
+/**
+ * DB connection setter.
+ * The interface should be implemented only for classes in which db connection cannot be injected through constructor
+ */
 interface DbConnectionSetterInterface
 {
     /**
