3eb3c1b19e6484299a14fda407e2e126a29e52d4
u.chebotok <chebotokyd@mchs.ru>
2023-04-03 00:06:26 +0300

    DBA-4: Separate data extractor and structure extractor

--- src/Service/DDL/ExtractorFactory.php
+++ src/Service/DDL/ExtractorFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Service\DDL;
 
+use App\Configuration\ExportDbConfiguration;
+use App\Exception\Service\DDL\DataExtractorNotFound;
 use App\Exception\Service\DDL\InvalidStructureExtractorInterface;
 use App\Exception\Service\DDL\StructureExtractorNotFound;
 use App\Service\DbConnectionSetterInterface;
@@ -19,8 +21,6 @@ class ExtractorFactory
 {
     /**
      * Available extractors.
-     *
-     * @var array<array-key, DbStructureExtractorInterface>
      */
     private array $extractors = [];
 
@@ -34,11 +34,18 @@ class ExtractorFactory
     public function __construct(Traversable $extractors)
     {
         foreach ($extractors as $extractor) {
-            if (!$extractor instanceof DbStructureExtractorInterface) {
+            if (!$extractor instanceof DbStructureExtractorInterface || !$extractor instanceof DbDataExtractorInterface) {
                 throw InvalidStructureExtractorInterface::byInterface(DbStructureExtractorInterface::class);
             }
 
-            $this->extractors[$extractor->getDbDriver()] = $extractor;
+            if ($extractor instanceof DbStructureExtractorInterface) {
+                $this->extractors[$extractor->getDbDriver()][DbStructureExtractorInterface::class] = $extractor;
+                continue;
+            }
+
+            if ($extractor instanceof DbDataExtractorInterface) {
+                $this->extractors[$extractor->getDbDriver()][DbDataExtractorInterface::class] = $extractor;
+            }
         }
     }
 
@@ -52,18 +59,48 @@ class ExtractorFactory
      * @throws InvalidStructureExtractorInterface
      * @throws StructureExtractorNotFound
      */
-    public function createExtractor(Connection $connection): DbStructureExtractorInterface
+    public function createStructureExtractor(Connection $connection): DbStructureExtractorInterface
     {
-        if (!isset($this->extractors[$connection->getDriver()::class])) {
+        if (!isset($this->extractors[$connection->getDriver()::class][DbStructureExtractorInterface::class])) {
             throw StructureExtractorNotFound::byDbDriverName($connection->getDriver()::class);
         }
 
-        $extractor = $this->extractors[$connection->getDriver()::class];
+        /** @var DbStructureExtractorInterface $extractor */
+        $extractor = $this->extractors[$connection->getDriver()::class][DbStructureExtractorInterface::class];
         if (!$extractor instanceof DbConnectionSetterInterface) {
             throw InvalidStructureExtractorInterface::byInterface(DbConnectionSetterInterface::class);
         }
 
         $extractor->setDbConnection($connection);
+
+        return $extractor;
+    }
+
+    /**
+     * Create db structure extractor based on db connection.
+     *
+     * @param Connection            $connection
+     * @param ExportDbConfiguration $configuration
+     *
+     * @return DbDataExtractorInterface
+     *
+     * @throws DataExtractorNotFound
+     * @throws InvalidStructureExtractorInterface
+     */
+    public function createDataExtractor(Connection $connection, ExportDbConfiguration $configuration): DbDataExtractorInterface
+    {
+        if (!isset($this->extractors[$connection->getDriver()::class][DbDataExtractorInterface::class])) {
+            throw DataExtractorNotFound::byDbDriverName($connection->getDriver()::class);
+        }
+
+        /** @var DbDataExtractorInterface $extractor */
+        $extractor = $this->extractors[$connection->getDriver()::class][DbDataExtractorInterface::class];
+        if (!$extractor instanceof DbConnectionSetterInterface) {
+            throw InvalidStructureExtractorInterface::byInterface(DbConnectionSetterInterface::class);
+        }
+
+        $extractor->setDbConnection($connection);
+        $extractor->setConfiguration($configuration);
 
         return $extractor;
     }
