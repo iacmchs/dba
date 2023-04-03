cd5a123f097064a8723a1e972a337694badbe85e
u.chebotok <chebotokyd@mchs.ru>
2023-04-03 08:45:42 +0300

    DBA-4: Rename exception InvalidStructureExtractorInterface to  InvalidExtractorInterfaceException and move file info to class info

--- src/Exception/Service/DDL/InvalidExtractorInterfaceException.php
+++ src/Exception/Service/DDL/InvalidExtractorInterfaceException.php
@@ -1,23 +1,22 @@
 <?php
 
-/**
- * @file
- * Implementation of an exception that should be thrown if a instance of extractor doesn't implement a necessary
- * interface.
- */
-
 declare(strict_types=1);
 
 namespace App\Exception\Service\DDL;
 
 use Exception;
 
-class InvalidStructureExtractorInterface extends Exception
+/**
+ * Implementation of an exception that should be thrown if a instance of extractor doesn't implement a necessary
+ * interface.
+ */
+class InvalidExtractorInterfaceException extends Exception
 {
     /**
      * Create exception instance with a prepared message.
      *
      * @param string $interface
+     *
      * @return self
      */
     public static function byInterface(string $interface): self
